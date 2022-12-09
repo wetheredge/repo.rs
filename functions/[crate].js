2967c57493576c7e78930ea0a0e3d5b0e5585dfd
William Etheredge <me@wetheredge.com>
2022-12-08 20:17:27 -0600

    Set User-Agent

--- functions/[crate].js
+++ functions/[crate].js
@@ -1,7 +1,12 @@
 export async function onRequestGet({ params: { crate } }) {
 	const url = `https://crates.io/api/v1/crates/${crate}`;
 	console.debug(`fetching '${url}'`);
-	const response = await fetch(url).then(resp => resp.json());
+	const response = await fetch(url, {
+		headers: {
+			'User-Agent': 'repo.rs',
+		},
+	}).then(resp => resp.json());
+
 	const repository = response?.crate?.repository;
 	console.debug(`redirecting to '${repository}'`);
 
