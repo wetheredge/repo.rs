13288f1c8e54f48c6ae3422a8280f6282683e5af
William Etheredge <me@wetheredge.com>
2022-12-08 20:04:14 -0600

    Enable logging

--- functions/[crate].js
+++ functions/[crate].js
@@ -1,5 +1,7 @@
 export async function onRequestGet({ params: { crate } }) {
-	const { crate: { repository } } = await fetch(`https://crates.io/api/v1/crates/${crate}`);
+	const response = await fetch(`https://crates.io/api/v1/crates/${crate}`);
+	console.info(JSON.stringify(response));
+	const { crate: { repository } } = response;
 
 	return new Response(null, {
 		status: 302,
