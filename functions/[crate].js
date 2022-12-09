6bb586a9d009fe877f5d0fd1621129d65dceea0b
William Etheredge <me@wetheredge.com>
2022-12-08 20:11:14 -0600

    Extract body

--- functions/[crate].js
+++ functions/[crate].js
@@ -1,7 +1,9 @@
 export async function onRequestGet({ params: { crate } }) {
-	const response = await fetch(`https://crates.io/api/v1/crates/${crate}`);
-	console.info(JSON.stringify(response));
-	const { crate: { repository } } = response;
+	const url = `https://crates.io/api/v1/crates/${crate}`;
+	console.debug(`fetching '${url}'`);
+	const response = await fetch(url).then(resp => resp.json());
+	const repository = response?.crate?.repository;
+	console.debug(`redirecting to '${repository}'`);
 
 	return new Response(null, {
 		status: 302,
